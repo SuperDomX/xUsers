a27482d6194b4a3573fc371aa38a8ea83ec044a3
xopherdeep <heylisten@xtiv.net>
2014-08-16 01:04:09 -0700

    xUsers/ Automated Nightly Push

--- xUsers.php
+++ xUsers.php
@@ -4,7 +4,7 @@
  * @author heylisten@xtiv.net
  * @name Users
  * @desc User Management
- * @version v1.1.0
+ * @version v1.1.1
  * @icon Contacts2.png
  * @mini users
  * @link users
@@ -31,6 +31,51 @@
   					'name'			=> array('Type' => 'varchar(255)'),
   					'picture_src'	=> array('Type' => 'varchar(255)'),
   					'stripe_id'		=> array('Type' => 'varchar(255)')
+				),
+
+				'user_addresses' => array(
+					'user_id'    => array('Type' => 'int(8)'),
+					'address_id' => array('Type' => 'int(8)')
+				),
+
+				'Addresses' => array(
+					// Primary Street Line [VarChar]
+					'primary_street_line'  => array('Type' => 'varchar(100)'),
+					'second_street_line'   => array('Type' => 'varchar(100)'),
+					
+					// Street Number [Int]
+					'street_number'        => array('Type' => 'int(8)'),
+					// Street Number Suffix [VarChar] - A~Z 1/3 1/2 2/3 3/4 etc
+					'street_number_suffix' => array('Type' => 'varchar(25)'),
+					// Street Name [VarChar]
+					'street_name'          => array('Type' => 'varchar(25)'),
+					// Street Type [VarChar] - Street, Road, Place etc.
+					'street_type'          => array('Type' => 'varchar(25)'),
+					// Street Direction [VarChar] - N, NE, E, SE, S, SW, W, NW
+					'street_direction'     => array('Type' => 'varchar(5)'),
+					
+					// Address Type [VarChar] - For example Apartment, Suite, Office, Floor, Building etc.
+					'address_type'         => array('Type' => 'varchar(50)'),
+					// Address Type Identifier [VarChar] - For instance the apartment number, suite, office or floor number or building identifier.
+					'address_type_id'      => array('Type' => 'varchar(50)'),
+					
+					// Minor Municipality (Village/Hamlet) [VarChar]
+					'municipality_minor'   => array('Type' => 'varchar(25)'),
+					
+					// Major Municipality (Town/City) [VarChar]
+					'municipality_major'   => array('Type' => 'varchar(50)'),
+					
+					// Governing District (Province, State, County) [VarChar]
+					'district'             => array('Type' => 'varchar(25)'),
+
+					// Governing District (County) [VarChar]
+					'district_sub'         => array('Type' => 'varchar(50)'),
+					
+					// Postal Area (Postal Code/Zip/Postcode)[VarChar]
+					'postal'               => array('Type' => 'varchar(25)'),
+
+					// Country [VarChar]
+					'country'              => array('Type' => 'varchar(50)')
 				)
 			);
 		}
@@ -39,8 +84,8 @@
 			$q = $sdx->q();
 
 			// Get the content for the page.
-			$url = substr($_SERVER['REQUEST_URI'],1);
-
+			$url   = substr($_SERVER['REQUEST_URI'],1);
+			
 			$parts = explode('/',$url);
 
 			// Is this a user name?
@@ -63,14 +108,43 @@
 
 					if(file_exists($file) ){
 						$user_x[$v['see']][$dir] = $v;
-					}
-
+					} 
 				}
 
 				$sdx->set('user_x',$user_x);
 			}
 
 
+		}
+
+		function idEmail($e){
+			$e['email']	  = $e;
+			$q            = $this->q();
+			$id           = $q->Select('id','Users',$e);
+			$id           = ( empty($id[0]['id']) ) ? $q->Insert('Users', $e) : $id[0]['id']; 
+
+			return array(
+				'success' => $id,
+				'error'   => $q->error
+			); 
+		}
+
+		protected function idAddress($a,$user_id){
+			$q            = $this->q();
+			$id           = $q->Select('id','Addresses',$a);
+			$id           = ( empty($id[0]['id']) ) ? $q->Insert('Addresses', $a) : $id[0]['id']; 
+
+			$w['user_id']    = $user_id;
+			$w['address_id'] = $id;
+
+			// User address not yet linked to user, so link it. 
+			if( empty( $q->Select('id','user_addresses', $w) ) )
+				$q->Insert('user_addresses', $w);
+
+			return array(
+				'success' => $id,
+				'error'   => $q->error
+			); 
 		}
 
 		function index(){
